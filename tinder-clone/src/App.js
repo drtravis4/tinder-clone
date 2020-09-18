4d62555d57bdbbcad741ef7e124fe7d61954d63c
Travis Bunn <drtravis4@gmail.com>
2020-09-18 14:46:58 -0500

    started react act for tinder clone

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from './Header';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Lets build the tinder clone app</h1>
+   <Header />
+   
+   
+    </div>
+  );
+  }
+export default App;
