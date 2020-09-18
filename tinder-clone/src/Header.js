7e92186dc316a2d70b70f7a97cfb01ee1efa839d
Travis Bunn <drtravis4@gmail.com>
2020-09-18 15:44:35 -0500

    finished header with icons

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 function Header() {
-    return (
-        <div>
-            <h2>i am a header</h2>
-        </div>
-    )
+  return (
+    <div className="header">
+      <PersonIcon className="header__icon" fontSize="large"/>
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinder logo"
+      />
+      <ForumIcon className="header__icon" fontSize="large"className="header__icon" />
+    </div>
+  );
 }
-export default Header
+export default Header;
