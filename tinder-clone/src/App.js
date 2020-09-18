74d1963d32d2b8794f1a2fd6adaf76ac10866d3e
Travis Bunn <drtravis4@gmail.com>
2020-09-18 16:34:30 -0500

    added the routes for the chatpage and home page

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
-import Header from './Header';
+import Header from "./Header";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
- <h1>Lets build the tinder clone app</h1>
-   <Header />
-   
-   
+      <Header />
+      <Router>
+        <switch>
+          <Route path="/chat">
+            <h1>I am chatpage</h1>
+          </Route>
+          <Route path="/">
+            <h1>i am homepage</h1>
+          </Route>
+        </switch>
+      </Router>
     </div>
   );
-  }
+}
 export default App;
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -2,16 +2,26 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
+
 function Header() {
   return (
     <div className="header">
-      <PersonIcon className="header__icon" fontSize="large"/>
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
       <img
         className="header__logo"
         src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
         alt="tinder logo"
       />
-      <ForumIcon className="header__icon" fontSize="large"className="header__icon" />
+      <IconButton>
+        <ForumIcon
+          className="header__icon"
+          fontSize="large"
+          className="header__icon"
+        />
+      </IconButton>
     </div>
   );
 }
